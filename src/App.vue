59d1ed93faee4f791288039408d7d307f0c3f937
Noguchi-maron <mimikkku1radi@gmail.com>
2021-08-18 07:06:08 +0900

    名前の入力欄を追加

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
 <v-app>
   <v-navigation-drawer app mx-auto my-0>
-    <v-btn text>TopPage</v-btn><br>
-    <v-btn text>About</v-btn>
+    <v-btn text block>TopPage</v-btn><br>
+    <v-btn text block>About</v-btn>
   </v-navigation-drawer>
 
   <v-app-bar app>
@@ -15,11 +15,15 @@
     <!-- Provides the application the proper gutter -->
     <v-container fluid>
       <v-form>
-      <v-col sm6>
-      <v-text-field v-model="msg" label="Name" clearable>
+      <v-col md="4" sm="6">
+      <v-text-field v-model="msg" label="FirstName" clearable>
       </v-text-field>
       </v-col>
-      <v-btn elevation="2" outlined class="font-weight-bold">
+      <v-col md="4" sm="6">
+      <v-text-field v-model="msg" label="LastName" clearable>
+      </v-text-field>
+      </v-col>
+      <v-btn elevation="0" outlined class="font-weight-bold">
         保存
       </v-btn>
 
